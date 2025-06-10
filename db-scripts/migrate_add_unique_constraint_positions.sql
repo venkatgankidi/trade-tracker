7d049f4ddfa8818d397acd985a6db09503206604
Venkat gankidi <venkat.gankidi@gmail.com>
2025-06-10 17:26:57 -0400

    remove obsolete migration scripts for positions and option trades

--- db-scripts/migrate_add_unique_constraint_positions.sql
+++
@@ -1,3 +0,0 @@
--- Add a unique constraint to ensure only one position per ticker/platform/position_type (open or closed) exists
-ALTER TABLE positions
-ADD CONSTRAINT unique_ticker_platform_positiontype UNIQUE (ticker, platform_id, position_type);
